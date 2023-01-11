c1da1fc6669373f2f2102ddf814eed9dfce29a36
imprisoner <69797749+imprisoner@users.noreply.github.com>
2023-01-12 02:53:28 +0500

    scss task added

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,1 +1,10 @@
-export {default as pug} from './gulpfile/pug.mjs';
+import gulp from 'gulp'
+
+// tasks imports
+import pug from './gulpfile/pug.mjs';
+import scss from './gulpfile/scss.mjs';
+
+const { series, parallel, watch } = gulp
+
+export const dev = () => watch(pug, scss)
+export default series(pug, scss)
---
+++ gulpfile/scss.mjs
@@ -0,0 +1,19 @@
+import gulp from 'gulp'
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+import sourcemaps from 'gulp-sourcemaps'
+import autoprefixer from 'gulp-autoprefixer'
+
+const {src, dest} = gulp
+const sass = gulpSass(dartSass)
+
+export default function scss(done) {
+    src('src/scss/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(sourcemaps.write())
+      .pipe(dest('dist/css/'))
+
+    done()
+}
