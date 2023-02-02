9da75a1f5588648eff8a69f7fa10262ba373ec85
imprisoner <continentalresident@yandex.ru>
2023-02-02 11:08:50 +0400

    added node_modules to gulp-scss

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,8 +1,8 @@
-import gulp from 'gulp'
-import dartSass from 'sass'
-import gulpSass from 'gulp-sass'
-import sourcemaps from 'gulp-sourcemaps'
-import autoprefixer from 'gulp-autoprefixer'
+import gulp from "gulp"
+import dartSass from "sass"
+import gulpSass from "gulp-sass"
+import sourcemaps from "gulp-sourcemaps"
+import autoprefixer from "gulp-autoprefixer"
 import { GLOBS } from "../config.js"
 
 const { src, dest } = gulp
@@ -11,7 +11,9 @@ const sass = gulpSass(dartSass)
 export default function scss(done) {
   src(GLOBS.SCSS.SRC)
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      includePaths: ["./node_modules"]
+    }).on("error", sass.logError))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write())
     .pipe(dest(GLOBS.SCSS.DEST))
