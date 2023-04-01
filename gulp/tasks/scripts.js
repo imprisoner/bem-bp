b862530c2bbfab2edebc30eb090b62ada416259e
imprisoner <continentalresident@yandex.ru>
2023-04-01 16:25:31 +0400

    test needed

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,7 +2,8 @@ import gulp from 'gulp'
 import webpack from 'webpack-stream'
 import webpackConfig from '../webpack.config.js'
 import { GLOBS } from "../config.js"
-
+import uglify from "gulp-uglify"
+import rename from "gulp-rename"
 const { src, dest } = gulp
 
 export function scriptsPages(done) {
@@ -11,7 +12,17 @@ export function scriptsPages(done) {
       config: webpackConfig
     }))
     .pipe(dest(GLOBS.SCRIPTS_PAGES.DEST))
-
+    .pipe(
+			uglify()
+		)
+		.on('error', function (err) { console.log(err.toString()); this.emit('end'); })
+		.pipe(
+			rename({
+				extname: ".min.js"
+			})
+		)
+    .pipe(dest(GLOBS.SCRIPTS_PAGES.DEST))
+    
     done()
 }
 
--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -4,6 +4,9 @@ import gulpSass from "gulp-sass"
 import sourcemaps from "gulp-sourcemaps"
 import autoprefixer from "gulp-autoprefixer"
 import { GLOBS } from "../config.js"
+import rename from "gulp-rename"
+import cleanCss from "gulp-clean-css"
+import groupMediaQueries from "gulp-group-css-media-queries"
 
 const { src, dest } = gulp
 const sass = gulpSass(dartSass)
@@ -11,11 +14,19 @@ const sass = gulpSass(dartSass)
 export default function scss(done) {
   src(GLOBS.SCSS.SRC)
     .pipe(sourcemaps.init())
+    .pipe(groupMediaQueries())
     .pipe(sass({
       includePaths: ["./node_modules"]
     }).on("error", sass.logError))
-    .pipe(autoprefixer())
+    .pipe(autoprefixer({
+      cascade: true
+    }))
     .pipe(sourcemaps.write())
+    .pipe(dest(GLOBS.SCSS.DEST))
+    .pipe(cleanCss())
+    .pipe(rename({
+      extname: ".min.css"
+    }))
     .pipe(dest(GLOBS.SCSS.DEST))
 
   done()
